dfc200abc499a981257d55d96ddaf441b9fead54
stendarr <stendarr@users.noreply.github.com>
2023-04-29 18:20:28 +0200

    add icons and make site installable

---
+++ sw.js
@@ -0,0 +1,19 @@
+const cacheName = 'sitecache';
+const filesToCache = ['/', '/chart.min.js', '/index.html', 'manifest.json', 'sw.js'];self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+    );
+  });
